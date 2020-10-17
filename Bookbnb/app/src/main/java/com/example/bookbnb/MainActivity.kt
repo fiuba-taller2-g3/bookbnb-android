2ab1a59c25b4cc7a78eb365cef1aa09a4e052870
Pablo <pablo.inoriza@despegar.com>
2020-10-17 18:25:17 -0300

    Agrego el toolbar

--- Bookbnb/app/src/main/java/com/example/bookbnb/MainActivity.kt
+++ Bookbnb/app/src/main/java/com/example/bookbnb/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.bookbnb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAppBar()
+    }
+
+    private fun initAppBar() {
+        setSupportActionBar(app_toolbar as Toolbar)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
