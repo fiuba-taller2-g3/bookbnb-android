68eb4b73d8e20ca8a5514e5ad23d65a20d2789a4
Pablo <pablo.inoriza@despegar.com>
2020-11-21 22:54:36 -0300

    Aplico una pequeña corrección en los radio buttons

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/forms/InputField.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/forms/InputField.kt
@@ -3,6 +3,10 @@ package com.fiuba.bookbnb.forms
 import org.apache.commons.lang3.StringUtils
 
 enum class InputField(private val postfixTextEmptyValidator: String) {
+
+    /* TODO: La idea sería almacenar estos textos en el string.xml, ya que si en algún momento se opta por
+    *   agregar un nuevo idioma en la app, no hay posibilidad de traducirlos */
+
     NAME("un nombre"),
     SURNAME("un apellido"),
     EMAIL("un correo electrónico"),
