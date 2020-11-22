f3aea15513179585800eee05c503203c29e71a58
Pablo <pablo.inoriza@despegar.com>
2020-11-22 17:58:42 -0300

    Agrego un timer en la confirmación del registro

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/misc/ConfirmationFragment.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/misc/ConfirmationFragment.kt
@@ -1,10 +1,12 @@
 package com.fiuba.bookbnb.ui.fragments.misc
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.navigation.fragment.navArgs
 import com.fiuba.bookbnb.R
 import com.fiuba.bookbnb.ui.fragments.BaseFragment
+import com.fiuba.bookbnb.ui.navigation.NavigationManager
 import kotlinx.android.synthetic.main.bookbnb_confirmation_fragment.*
 
 class ConfirmationFragment : BaseFragment(R.layout.bookbnb_confirmation_fragment) {
@@ -23,5 +25,11 @@ class ConfirmationFragment : BaseFragment(R.layout.bookbnb_confirmation_fragment
         check_img.setImageResource(img)
         confirmation_title.text = title
         confirmation_subtitle.text = getString(subtitle)
+
+        Handler().postDelayed({ NavigationManager.popBackStack() }, MILLISECONDS_WAIT)
+    }
+
+    companion object {
+        private const val MILLISECONDS_WAIT : Long = 3000
     }
 }
