31b07a8d5d86b4432335edef8d7b567e9398ee31
Pablo <pablo.inoriza@despegar.com>
2020-12-06 00:38:48 -0300

    Agrego el funcionamiento del login con facebook

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/login/StartLoginFragment.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/login/StartLoginFragment.kt
@@ -1,9 +1,12 @@
 package com.fiuba.bookbnb.ui.fragments.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.facebook.*
+import com.facebook.login.LoginResult
 import com.fiuba.bookbnb.R
-import com.fiuba.bookbnb.misc.ButtonItem
 import com.fiuba.bookbnb.ui.fragments.BaseFragment
 import com.fiuba.bookbnb.ui.navigation.NavigationManager
 import kotlinx.android.synthetic.main.bookbnb_button.*
@@ -11,11 +14,54 @@ import kotlinx.android.synthetic.main.bookbnb_start_login_fragment.*
 
 class StartLoginFragment : BaseFragment(R.layout.bookbnb_start_login_fragment) {
 
+    private val callbackManager by lazy { CallbackManager.Factory.create() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         button.text = getString(R.string.start_login_email_text)
         button.setOnClickListener { NavigationManager.moveForward(StartLoginFragmentDirections.actionStartLoginFragmentToLoginFragment()) }
         not_register_text.setOnClickListener { NavigationManager.moveForward(StartLoginFragmentDirections.actionStartLoginFragmentToRegisterFragment()) }
-        buttons_container.addView(ButtonItem(requireContext(), "Iniciar con Facebook", R.drawable.facebook_button_ripple))
+        facebook_login_custom_button.setOnClickListener { facebook_login_button.performClick() }
+        onFacebookLoginResult()
+    }
+
+    private fun onFacebookLoginResult() {
+        facebook_login_button.fragment = this
+        facebook_login_button.setPermissions(listOf(EMAIL));
+        facebook_login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+
+            override fun onSuccess(result: LoginResult) {
+                Log.i("LOGIN", result.accessToken.token)
+                Log.i("LOGIN", result.accessToken.userId)
+                val profileTracker = object : ProfileTracker() {
+                    override fun onCurrentProfileChanged(oldProfile: Profile?, currentProfile: Profile?) {
+                        this.stopTracking()
+                        Profile.setCurrentProfile(currentProfile)
+                        currentProfile?.let {
+                            Log.i("LOGIN", it.firstName)
+                            Log.i("LOGIN", it.lastName)
+                        }
+                    }
+                }
+                profileTracker.startTracking()
+            }
+
+            override fun onCancel() {
+                // Do Nothing
+            }
+
+            override fun onError(error: FacebookException) {
+                // Do Nothing
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    companion object {
+        private const val EMAIL = "email"
     }
 }
