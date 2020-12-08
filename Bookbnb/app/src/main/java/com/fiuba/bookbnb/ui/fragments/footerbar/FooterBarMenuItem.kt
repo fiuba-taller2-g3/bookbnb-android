764107e4daa0e279fe6cb04c90b4b95b327c6ca5
Pablo <pablo.inoriza@despegar.com>
2020-12-07 21:15:32 -0300

    Coloco detalles visuales en la footer bar

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/footerbar/FooterBarMenuItem.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/footerbar/FooterBarMenuItem.kt
@@ -1,7 +1,6 @@
 package com.fiuba.bookbnb.ui.fragments.footerbar
 
 import android.content.Context
-import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.ViewGroup
