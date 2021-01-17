1b3d392b1dbff3533a945737c72e11b9f36a4cd3
Pablo <pablo.inoriza@despegar.com>
2021-01-17 18:54:59 -0300

    Concluyo con la pantalla de home

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/home/HomeFragment.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/home/HomeFragment.kt
@@ -2,15 +2,34 @@ package com.fiuba.bookbnb.ui.fragments.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.fiuba.bookbnb.R
 import com.fiuba.bookbnb.ui.fragments.BaseFragment
 import com.fiuba.bookbnb.ui.fragments.footerbar.FooterBarButtons
+import com.fiuba.bookbnb.ui.navigation.NavigationManager
+import com.fiuba.bookbnb.user.UserManager
+import kotlinx.android.synthetic.main.bookbnb_home_fragment.*
 
 class HomeFragment : BaseFragment(R.layout.bookbnb_home_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        search_container.isVisible = UserManager.isUserLogged()
+        login_button.isVisible = !UserManager.isUserLogged()
+        bkbnb_form_title.setText(getTitle())
+        bkbnb_form_subtitle.setText(getSubtitle())
 
+        login_button.setOnClickListener {
+            NavigationManager.moveGlobalTo(R.id.startProfile)
+        }
+    }
+
+    private fun getTitle() : Int {
+        return if (UserManager.isUserLogged()) R.string.home_search_title else R.string.home_search_title_not_logged
+    }
+
+    private fun getSubtitle() : Int {
+        return if (UserManager.isUserLogged()) R.string.home_search_subtitle else R.string.home_search_subtitle_not_logged
     }
 
     override val shouldShowToolbar: Boolean
