14fe8a8313ba25d6da47364fce54753f3637b313
Pablo <pablo.inoriza@despegar.com>
2021-02-11 17:07:12 -0300

    Agrego el contador de imágenes

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/publish/publishView/PublishViewFragment.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/publish/publishView/PublishViewFragment.kt
@@ -1,8 +1,10 @@
 package com.fiuba.bookbnb.ui.fragments.publish.publishView
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager.widget.ViewPager
 import com.fiuba.bookbnb.R
 import com.fiuba.bookbnb.ui.fragments.BaseFragment
 import kotlinx.android.synthetic.main.bookbnb_publish_view_fragment.*
@@ -15,6 +17,26 @@ class PublishViewFragment : BaseFragment(R.layout.bookbnb_publish_view_fragment)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view_pager.adapter = ViewPagerAdapter(publishData.images)
+        setCurrentPositionImageText(view_pager.currentItem)
+
+        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                // Do nothing
+            }
+
+            override fun onPageSelected(position: Int) =  setCurrentPositionImageText(position)
+
+            override fun onPageScrollStateChanged(state: Int) {
+                // Do nothing
+            }
+
+        })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setCurrentPositionImageText(position: Int) {
+        img_number.text = "${position + 1}/${publishData.images.size}"
     }
 
     override fun shouldClearInputsWhenBackPressed(): Boolean = false
