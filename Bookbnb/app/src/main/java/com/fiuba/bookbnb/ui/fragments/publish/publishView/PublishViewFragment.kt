085449a58acb8f327007ceae5c79c5e309f3e891
Pablo <pablo.inoriza@despegar.com>
2021-02-14 00:47:27 -0300

    Agrego el texto del sumario del alojamiento en la vista de publicación

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/publish/publishView/PublishViewFragment.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/publish/publishView/PublishViewFragment.kt
@@ -8,6 +8,7 @@ import androidx.viewpager.widget.ViewPager
 import com.fiuba.bookbnb.R
 import com.fiuba.bookbnb.ui.fragments.BaseFragment
 import kotlinx.android.synthetic.main.bookbnb_publish_view_fragment.*
+import java.util.*
 
 class PublishViewFragment : BaseFragment(R.layout.bookbnb_publish_view_fragment) {
 
@@ -15,6 +16,7 @@ class PublishViewFragment : BaseFragment(R.layout.bookbnb_publish_view_fragment)
     private val publishData by lazy { navArguments.publishData }
     private val userData by lazy { navArguments.userData }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view_pager.adapter = ViewPagerAdapter(publishData.images)
@@ -37,12 +39,44 @@ class PublishViewFragment : BaseFragment(R.layout.bookbnb_publish_view_fragment)
         publish_title.text = publishData.title
         publish_location.text = with(publishData.location) { "${city}, $country" }
         host_name.text = getString(R.string.publish_view_host_name, "${userData.name} ${userData.surname}")
+        stay_type.text = "${publishData.type} \u2022 ${publishData.availabilityType}"
+        stay_summary.text = buildStaySummary()
     }
 
     @SuppressLint("SetTextI18n")
     private fun setCurrentPositionImageText(position: Int) {
         img_number.text = "${position + 1}/${publishData.images.size}"
     }
+
+    private fun buildStaySummary(): String {
+        val staySummary = StringJoiner("  •  ")
+        staySummary.apply {
+            //add(buildGuestText())
+            add(buildBedroomText())
+            add(buildBedsText())
+            add(buildBathroomText())
+        }
+
+        return staySummary.toString()
+    }
+
+    private fun buildGuestText() = getString(
+        if (isSingular(publishData.guests.toInt())) R.string.publish_view_guest_singular_label else R.string.publish_view_guest_plural_label, publishData.guests
+    )
+
+    private fun buildBedsText() = getString(
+        if (isSingular(publishData.beds.toInt())) R.string.publish_view_beds_singular_label else R.string.publish_view_beds_plural_label, publishData.beds
+    )
+
+    private fun buildBathroomText() = getString(
+        if (isSingular(publishData.bathrooms.toInt())) R.string.publish_view_bathroom_singular_label else R.string.publish_view_bathroom_plural_label, publishData.bathrooms
+    )
+
+    private fun buildBedroomText() = getString(
+        if (isSingular(publishData.bedrooms.toInt())) R.string.publish_view_bedroom_singular_label else R.string.publish_view_bedroom_plural_label, publishData.bedrooms
+    )
+
+    private fun isSingular(number: Int) = number == 1
 
     override fun shouldClearInputsWhenBackPressed(): Boolean = false
 
