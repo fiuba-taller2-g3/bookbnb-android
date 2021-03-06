ba36d7c4185f6c7ecf184576d734fdd6b00edee8
Pablo <pablo.inoriza@despegar.com>
2021-03-06 19:20:20 -0300

    Agrego información de compra antes de confirmar

---
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/booking/BookingSelectDateFormFragment.kt
@@ -0,0 +1,39 @@
+package com.fiuba.bookbnb.ui.fragments.booking
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.fiuba.bookbnb.R
+import com.fiuba.bookbnb.forms.FormInputData
+import com.fiuba.bookbnb.forms.FormInputType
+import com.fiuba.bookbnb.ui.fragments.form.FormFragment
+import com.fiuba.bookbnb.ui.navigation.NavigationManager
+import com.fiuba.bookbnb.utils.DateUtils
+import kotlinx.android.synthetic.main.bookbnb_booking_info_field.*
+
+class BookingSelectDateFormFragment : FormFragment() {
+
+    private val navArguments by navArgs<BookingConfirmFormFragmentArgs>()
+    private val publishData by lazy { navArguments.publishData }
+
+    override fun getTitleTextRes(): Int = R.string.booking_dates_title_text
+    override fun getSubtitleTextRes(): Int = R.string.booking_dates_subtitle_text
+    override fun getButtonTextRes(): Int = R.string.next_button_text
+
+    override fun shouldClearInputsWhenBackPressed(): Boolean = false
+
+    override fun getInputList(): List<FormInputData> {
+        val startDate = DateUtils.getDateOutputFormat().parse(publishData.availabilityDates.startDate)
+        val endDate = DateUtils.getDateOutputFormat().parse(publishData.availabilityDates.endDate)
+
+        return listOf(
+            FormInputData(FormInputType.BOOKING_START_DATE, DateUtils.getDateOutputFormat().format(startDate!!), startDate, endDate!!),
+            FormInputData(FormInputType.BOOKING_END_DATE, DateUtils.getDateOutputFormat().format(endDate), startDate, endDate)
+        )
+    }
+
+    override fun setActionEventButton() {
+        NavigationManager.moveForward(BookingSelectDateFormFragmentDirections.actionBookingSelectDateFormFragmentToBookingConfirmFormFragment(publishData))
+    }
+
+}
