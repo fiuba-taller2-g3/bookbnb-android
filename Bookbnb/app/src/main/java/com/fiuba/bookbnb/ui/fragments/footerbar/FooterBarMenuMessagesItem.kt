86790f2c4275f49d7385d2cbabe246272afd36f4
Pablo <pablo.inoriza@despegar.com>
2021-03-09 00:13:06 -0300

    Corrijo el acceso al chat para usuario no logueado

--- Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/footerbar/FooterBarMenuMessagesItem.kt
+++ Bookbnb/app/src/main/java/com/fiuba/bookbnb/ui/fragments/footerbar/FooterBarMenuMessagesItem.kt
@@ -3,10 +3,12 @@ package com.fiuba.bookbnb.ui.fragments.footerbar
 import android.content.Context
 import com.fiuba.bookbnb.R
 import com.fiuba.bookbnb.ui.navigation.NavigationManager
+import com.fiuba.bookbnb.user.UserManager
 
 class FooterBarMenuMessagesItem(context: Context) : FooterBarMenuItem(context, R.string.footer_bar_messages_menu, R.drawable.ic_messages) {
 
     override fun setButtonListener() {
-        NavigationManager.moveGlobalToWithPopUpTo(R.id.seeChatList, R.id.homeFragment)
+        if (UserManager.isUserLogged()) NavigationManager.moveGlobalToWithPopUpTo(R.id.startChatList, R.id.homeFragment)
+        else NavigationManager.moveGlobalToWithPopUpTo(R.id.startChatLogin, R.id.homeFragment)
     }
 }
